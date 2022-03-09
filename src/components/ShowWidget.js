7b7d0ed7f3da3d6e508ba482dc2f6691abe248a1
harrdev <jasonharrphx@live.com>
2022-03-09 16:18:22 -0700

    delete widget functioning

--- src/components/ShowWidget.js
+++ src/components/ShowWidget.js
@@ -1,16 +1,36 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { deleteWidget, getWidgets } from "../api/widgets";
 
 const WidgetDetails = ({ widgets }) => {
-  const [widget, setWidget] = useState([]);
+  const [myWidget, setMyWidget] = useState([]);
   const thisWidget = useParams();
 
   // Filter useParams data to equal database widget data
   const widgetData = widgets.filter((widget) => widget.name === thisWidget.id);
   const w = widgetData[0];
 
+  const removeWidget = (w) => {
+    deleteWidget(w._id).then((res) => {
+      getWidgets()
+        .then((res) => {
+          let widgetsArray = [];
+          res.data.widget.map((widget) => {
+            return widgetsArray.push(widget);
+          });
+          setMyWidget(widgetsArray);
+        })
+        .catch((error) => {});
+    });
+  };
+
   return (
     <div className="container">
+      <div className="filmRemoveButton">
+        <button className="removeButton" onClick={() => removeWidget(w)}>
+          Delete Widget
+        </button>
+      </div>
       <p>Name: {w.name}</p>
       <p>Type: {w.type}</p>
       <p>Quantity: {w.quantity}</p>
